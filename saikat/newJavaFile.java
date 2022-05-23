14b4cbb930b028325a6a9015c5e537a15a6403f9
Sarkitdas <78489797+Sarkitdas@users.noreply.github.com>
2022-05-24 00:15:03 +0600

    Update newJavaFile.java

--- saikat/newJavaFile.java
+++ saikat/newJavaFile.java
@@ -1,11 +1,11 @@
 package saikat;
 
-public class newJavaFile 
+public class newJavaFile  //Another class
 {
-    public static void main(String args[]) 
+    public static void main(String args[])    //main class 
     {
-        newJava piu=new newJava();
-        piu.newJava();
+        newJava piu=new newJava();     //parent_class object_name =new parent_class  //new for creating instance class or allocating memory;
+        piu.newJava();     ///object_name.constructor from the parent class
         piu.newJava(10,25);
         piu.newJava(5.5,6.5);
     }
